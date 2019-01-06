bd074bfa4effdd7815ea2fc3d31334e98afc15bf
DESKTOP-TC7QSU2\Hrvoje <hsostaric@foi.hr>
2019-01-06 23:29:29 +0100

    Personal profile

--- src/Controller/PersonalAccountController.php
+++ src/Controller/PersonalAccountController.php
@@ -10,11 +10,25 @@ namespace App\Controller;
 
 
 use App\Entity\Korisnik;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_USER");
+ */
 class PersonalAccountController extends AbstractController
 {
-    public function perosnalAccount(Korisnik $korisnik){
-        return $this->render("",$korisnik);
+
+
+    /**
+     * @Route("/profil/{id}", name="user_personal");
+     */
+    public function perosnalAccount($id){
+        $logiraniKorisnik=$this->getDoctrine()->getManager()->getRepository(Korisnik::class)->findOneBy(['id'=>$id]);
+
+        return $this->render("personal/personalpage.html.twig",[
+            'user'=>$logiraniKorisnik,
+        ]);
     }
 }
