5f81a56e4925ca0a6b485d0a1e815c7f50c34759
DESKTOP-TC7QSU2\Hrvoje <hsostaric@foi.hr>
2019-01-07 06:38:58 +0100

    CRUD pt4

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,18 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_USER");
+ */
 class StudentController extends AbstractController
 {
     /**
-     * @Route("/student", name="student")
+     * @Route("/studenti", name="student_list")
      */
-    public function index()
+    public function PrikazStudenata(StudentRepository $repository, Request $request)
     {
-        return $this->render('student/index.html.twig', [
-            'controller_name' => 'StudentController',
+        if($request->isMethod('POST')){
+            return $this->redirectToRoute('kreiranje_studenta');
+        }
+        $popisStudenata=$repository->findAll();
+        return $this->render('student/studenti.html.twig', [
+            'students'=>$popisStudenata,
         ]);
+    }
+    /**
+     * @Route("/novi-student", name="kreiranje_studenta")
+     */
+    public function DodajStudenta(Request $request){
+        $greske=array();
+        $ime=$request->request->get('imeStudenta');
+        $prezime=$request->request->get('prezimeStudenta');
+        $status=$request->request->get('statusStudenta');
+        $email=$request->request->get('emailKorisnika');
+        $napomena=$request->request->get('napomena');
+        if ($request->isMethod('POST') ){
+            if(empty($ime) || empty($prezime) || empty($prezime) || empty($status) || empty($email)) $greske[]="Nisu popunjena sva polja";
+            else{
+                $em=$this->getDoctrine()->getManager();
+                $noviStudent = new Student();
+                $noviStudent->setIme($ime)->setPrezime($prezime)->setStatus($status)->setEmail($email)->setNapomena($napomena);
+                $em->persist($noviStudent);
+                $em->flush();
+
+                return $this->redirectToRoute('student_list');
+            }
+        }
+
+        return $this->render('student\novi.html.twig',[
+            'errors'=>$greske,
+        ]);
+    }
+
+    /**
+     * @Route('/studenti/{slug}/pridruzi-timu', name="student_to_team");
+     */
+    public function PridruziTimStudentu(Request $request){
+
+
     }
 }
